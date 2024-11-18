beac5fb607f41978185dd64b3126a1afe11ee0e7
sokoy <attysokoy11@gmail.com>
2024-11-17 23:44:56 -0800

    first

---
+++ app/src/main/java/com/example/project/Expense.java
@@ -0,0 +1,33 @@
+package com.example.project;
+
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.groupproject.databinding.ActivityIncomeBinding;
+import com.example.project.databinding.ActivityFinanceBinding;
+
+public class Expense extends AppCompatActivity {
+    private ActivityFinanceBinding binding;
+    private PreferenceManager preferenceManager;
+    private boolean validateIncomeDetails;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityFinanceBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setListener();
+    }
+
+    private void setListener() {
+
+        binding.buttonEnter.setOnClickListener(v -> onBackPressed());
+    }
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
