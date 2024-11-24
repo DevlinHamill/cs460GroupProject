dad0dff80bf5558bf1e6f3dd8f3888a0a637a5b7
Cassie Thun <thunc@cwu.edu>
2024-11-23 18:05:23 -0800

    Fixed a bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,6 @@ buildscript {
         classpath(libs.google.services)
     }
 }
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    dependencies {
-        classpath(libs.google.services)
-    }
-}
 plugins {
     alias(libs.plugins.androidApplication) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
